f261cc6ced3e60513ecd6800dcfcde715c1c205e
XCristhianXD <cristhianamador777@gmail.com>
2026-03-05 16:31:53 -0400

    nose xddddddd

--- test_executor.py
+++ test_executor.py
@@ -1,9 +1,10 @@
 from executor.executor import ejecutar_accion
 
 # Prueba abrir una app
-resultado = ejecutar_accion("ABRIR_APP", "firefox")
+resultado = ejecutar_accion("CERRAR_APP", "gnome-calculator")
 # firefox
 # gnome-calculator
+# APAGAR_EQUIPO
 
 print("Resultado:")
 print(resultado)
