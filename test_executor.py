47830272d420509f33610a4097c60c00a0f7cde6
XCristhianXD <cristhianamador777@gmail.com>
2026-03-05 07:51:07 -0400

    Agregacion de verificador para el codigo wwazaaaaa

---
+++ test_executor.py
@@ -0,0 +1,9 @@
+from executor.executor import ejecutar_accion
+
+# Prueba abrir una app
+resultado = ejecutar_accion("ABRIR_APP", "firefox")
+# firefox
+# gnome-calculator
+
+print("Resultado:")
+print(resultado)
